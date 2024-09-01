3c16a249cd6d2451c078bba2d49f76e37a0fcfb5
gozipizi <quentin.gourdel@ensiie.fr>
2024-09-01 12:50:45 +0200

    Rework niveau diplome

--- src/app/pages/register/register-form/diploma-input/single-diploma/single-diploma.component.ts
+++ src/app/pages/register/register-form/diploma-input/single-diploma/single-diploma.component.ts
@@ -30,8 +30,35 @@ export class SingleDiplomaComponent {
     }
 
     this.rawDiplomas.forEach(diploma => {
-      this.diplomas.push(diploma.intitule_de_la_specialite_du_diplome_et_options + ' - ' + diploma.niveau_du_diplome.replace('Niveau ', 'BAC + '));
+      this.diplomas.push(diploma.intitule_de_la_specialite_du_diplome_et_options + ' - ' + this.accurateReplace(diploma.niveau_du_diplome));
     });
+  }
+
+  accurateReplace(str: string) {
+
+    console.log('str', str);
+    let result = str;
+
+    /*
+    'Niveau 1' 'Master/Doctorat'
+    'Niveau 2' 'License'
+    'Niveau 3' 'BAC + 2'
+    'Niveau 4' 'BAC'
+    'Niveau 5' 'BEP/CAP'
+    */
+
+    if (str === 'Niveau 1') {
+      result = 'Master/Doctorat';
+    } else if (str === 'Niveau 2') {
+      result = 'License';
+    } else if (str === 'Niveau 3') {
+      result = 'BAC + 2';
+    } else if (str === 'Niveau 4') {
+      result = 'BAC/BAC PRO';
+    } else if (str === 'Niveau 5') {
+      result = 'BEP/CAP';
+    }
+    return result;
   }
 
 }
